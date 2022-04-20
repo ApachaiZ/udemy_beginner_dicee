15e67ca453a215c643fc2767ca1473a9d6c184fd
Dane SAN <dane.g.san@gmail.com>
2022-04-20 16:36:20 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/danesan/udemy_beginner_dicee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.danesan.udemy_beginner_dicee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+         title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
