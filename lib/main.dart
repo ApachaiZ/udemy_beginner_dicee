660ffb304e617e98019d7ea3573f93c44dae79e0
Dane SAN <dane.g.san@gmail.com>
2022-04-21 15:30:39 +0200

    Dices rolling !

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-         title: const Text('Dicee'),
+          title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
         body: const DicePage(),
@@ -15,11 +16,60 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int firstDiceFace = 1;
+  int secondDiceFace = 1;
+
+  void rollDice() {
+    setState(
+      () {
+        firstDiceFace = 1 + Random().nextInt(6);
+        secondDiceFace = 1 + Random().nextInt(6);
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: rollDice,
+                    child: Image.asset(
+                      "assets/images/dice$firstDiceFace.png",
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(
+                    onPressed: rollDice,
+                    child: Image.asset(
+                      "assets/images/dice$secondDiceFace.png",
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
